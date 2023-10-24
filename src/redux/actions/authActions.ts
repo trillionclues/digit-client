abe129a634ef44cdffe9a5550a919eb38aefef8a
Trillionclues <trillionclues@gmail.com>
2023-10-24 10:54:32 +0100

    store token in storage

--- src/redux/actions/authActions.ts
+++ src/redux/actions/authActions.ts
@@ -1,18 +1,18 @@
-import { login, logout, setUser } from "../features/authSlice";
-import { AppDispatch } from "../store/store";
+// import { login, logout, setUser } from "../features/authSlice";
+// import { AppDispatch } from "../store/store";
 
-// login user
-export const loginUser = (token: string) => async (dispatch: AppDispatch) => {
-  dispatch(login(token));
-};
+// // login user
+// export const loginUser = (token: string) => async (dispatch: AppDispatch) => {
+//   dispatch(login(token));
+// };
 
-// logout user
-export const logOutUser = () => async (dispatch: AppDispatch) => {
-  dispatch(logout());
-};
+// // logout user
+// export const logOutUser = () => async (dispatch: AppDispatch) => {
+//   dispatch(logout());
+// };
 
-// set user
-export const setCurrentUser =
-  (userData: any) => async (dispatch: AppDispatch) => {
-    dispatch(setUser(userData));
-  };
+// // set user
+// export const setCurrentUser =
+//   (userData: any) => async (dispatch: AppDispatch) => {
+//     dispatch(setUser(userData));
+//   };
--- src/redux/services/setCookie.ts
+++ src/redux/services/setCookie.ts
@@ -1,14 +1,14 @@
-import { serialize } from "cookie";
-import { NextApiResponse } from "next";
+// import { serialize } from "cookie";
+// import { NextApiResponse } from "next";
 
-export function setCookie(res: NextApiResponse, name: string, value: string) {
-  const cookie = serialize(name, value, {
-    httpOnly: true,
-    maxAge: 7 * 24 * 60 * 60, // 7 days in seconds
-    sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
-    path: "/",
-  });
+// export function setCookie(res: NextApiResponse, name: string, value: string) {
+//   const cookie = serialize(name, value, {
+//     httpOnly: true,
+//     maxAge: 7 * 24 * 60 * 60, // 7 days in seconds
+//     sameSite: "lax",
+//     secure: process.env.NODE_ENV === "production",
+//     path: "/",
+//   });
 
-  res.setHeader("Set-Cookie", [cookie]);
-}
+//   res.setHeader("Set-Cookie", [cookie]);
+// }
