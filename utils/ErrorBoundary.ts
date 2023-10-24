1c6cc8fa4ea93a9016968f3f8ce0220371a60e5b
Trillionclues <trillionclues@gmail.com>
2023-10-24 10:57:02 +0100

    store token in storage

--- utils/ErrorBoundary.ts
+++ utils/ErrorBoundary.ts
@@ -1,25 +1,25 @@
-import React, { useState, ReactNode } from "react";
+// import React, { useState, ReactNode } from "react";
 
-// Define the props type
-interface ErrorBoundaryProps {
-  children: ReactNode;
-}
+// // Define the props type
+// interface ErrorBoundaryProps {
+//   children: ReactNode;
+// }
 
-function ErrorBoundary(props: ErrorBoundaryProps) {
-  const [hasError, setHasError] = useState(false);
+// function ErrorBoundary(props: ErrorBoundaryProps) {
+//   const [hasError, setHasError] = useState(false);
 
-  const componentDidCatch = (error: Error, info: React.ErrorInfo) => {
-    // Handle the error here
-    console.error(error, info);
-    setHasError(true);
-  };
+//   const componentDidCatch = (error: Error, info: React.ErrorInfo) => {
+//     // Handle the error here
+//     console.error(error, info);
+//     setHasError(true);
+//   };
 
-  if (hasError) {
-    // Render a fallback UI
-    return <div>Something went wrong.</div>;
-  }
+//   if (hasError) {
+//     // Render a fallback UI
+//     return <div>Something went wrong.</div>;
+//   }
 
-  return <>{props.children}</>;
-}
+//   return <>{props.children}</>;
+// }
 
-export default ErrorBoundary;
+// export default ErrorBoundary;
