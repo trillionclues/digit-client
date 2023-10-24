b9e9cc53f14628f243092a12fce9c681919d125f
Trillionclues <trillionclues@gmail.com>
2023-10-24 10:51:24 +0100

    store token in storage

--- src/redux/Providers/persistProvider.tsx
+++ src/redux/Providers/persistProvider.tsx
@@ -1,17 +1,17 @@
-"use client";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "../store/store";
+// "use client";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistor } from "../store/store";
 
-const isClient = typeof window !== `undefined` && !!window.document;
+// const isClient = typeof window !== `undefined` && !!window.document;
 
-export function Persistprovider({ children }: { children: React.ReactNode }) {
-  if (isClient) {
-    return (
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    );
-  }
+// export function Persistprovider({ children }: { children: React.ReactNode }) {
+//   if (isClient) {
+//     return (
+//       <PersistGate loading={null} persistor={persistor}>
+//         {children}
+//       </PersistGate>
+//     );
+//   }
 
-  return null;
-}
+//   return null;
+// }
